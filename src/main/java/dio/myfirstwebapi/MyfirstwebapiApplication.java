b0843d20f3115959965af533c8a714af1eda1b14
Ismael Barroso <mael-sf@hotmail.com>
2022-04-23 19:39:52 -0300

    first commit

---
+++ src/main/java/dio/myfirstwebapi/MyfirstwebapiApplication.java
@@ -0,0 +1,13 @@
+package dio.myfirstwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwebapiApplication.class, args);
+	}
+
+}
